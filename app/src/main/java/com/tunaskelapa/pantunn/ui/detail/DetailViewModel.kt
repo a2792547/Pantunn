dc6e884c16fb6283dd712eccc3758e9dfb6a2928
Gamal Abdul Aziz <a2792547@bangkit.academy>
2021-05-28 22:51:41 +0700

    Initial commit The prototype Pantunn app

---
+++ app/src/main/java/com/tunaskelapa/pantunn/ui/detail/DetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.tunaskelapa.pantunn.ui.detail
+
+import androidx.lifecycle.ViewModel
+
+class DetailViewModel: ViewModel() {
+}
---
+++ app/src/main/java/com/tunaskelapa/pantunn/ui/main/MainViewModel.kt
@@ -0,0 +1,6 @@
+package com.tunaskelapa.pantunn.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+}
